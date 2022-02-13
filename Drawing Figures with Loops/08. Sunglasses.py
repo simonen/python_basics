7d8602524195f9d68ba6375f176b0afc97b79750
simonen <si.petrov@yahoo.com>
2022-02-13 21:57:44 +0200

    Drawing Figures with Python

--- Drawing Figures with Loops/08. Sunglasses.py
+++ Drawing Figures with Loops/08. Sunglasses.py
@@ -1,2 +1,38 @@
 n = int(input())
 
+# First row
+
+for row in range(1):
+    for i in range(0, n * 2):
+        print("*", end="")
+    for j in range(0, n):
+        print(" ", end="")
+    for i in range(0, n * 2):
+        print("*", end="")
+    print()
+
+for row2 in range(0, n - 2):
+    print("*", end="")
+    for k in range(0, n * 2 - 2):
+        print("/", end="")
+    print("*", end="")
+    if row2 == (n - 1) // 2 - 1:
+        for m in range(0, n):
+            print("|", end="")
+    else:
+        for l in range(0, n):
+            print(" ", end="")
+    print("*", end="")
+    for o in range(0, n * 2 - 2):
+        print("/", end="")
+    print("*", end="")
+    print()
+
+for row in range(1):
+    for i in range(0, n * 2):
+        print("*", end="")
+    for j in range(0, n):
+        print(" ", end="")
+    for i in range(0, n * 2):
+        print("*", end="")
+    print()
