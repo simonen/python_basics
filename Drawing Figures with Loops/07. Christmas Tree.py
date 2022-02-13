faf6d36d75a334e7dc31c6c9de5575ea9bc3b747
simonen <si.petrov@yahoo.com>
2022-02-13 15:01:36 +0200

    Drawing Figures with Python

---
+++ Drawing Figures with Loops/07. Christmas Tree.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for row in range(0, n + 1):
+    for i in range(n - row):
+        print(" ", end="")
+    for j in range(0, row):
+        print("*", end="")
+    print(" | ", end="")
+    for k in range(0, row):
+        print("*", end="")
+    for p in range(n-row):
+        print(" ", end="")
+    print()
