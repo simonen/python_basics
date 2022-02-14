2068aa9afe329e2dbc5883deb3cd209cf3c7b950
simonen <si.petrov@yahoo.com>
2022-02-14 20:58:16 +0200

    Drawing Figures with Python

--- Drawing Figures with Loops/10. Diamond.py
+++ Drawing Figures with Loops/10. Diamond.py
@@ -15,15 +15,18 @@ for row in range(0, (n + 1) // 2):
         print("-" * 2 * row, end="")
     else:
         print("-" * (2 * row - 1), end="")
+    # Outer right body
     print("*", end="")
     print("-" * ((n + 1) // 2 - row - 1), end="")
     print()
 
 for row2 in range(1, (n + 1) // 2):
+    # Outer left body
     print("-" * row2, end="")
     print("*", end="")
     # Inner body
     print("-" * (n - 2 * row2 - 2), end="")
+    # Bottom vertex
     if row2 == (n + 1) // 2 - 1:
         if n % 2 == 0:
             print("*", end="")
@@ -31,5 +34,6 @@ for row2 in range(1, (n + 1) // 2):
             print("", end="")
     else:
         print("*", end="")
+    # Outer right body
     print("-" * row2, end="")
     print()
